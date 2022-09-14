7ada17d8ea95ed4ef2c4d50ef1d03d699aebf006
Connor De Meyer <connordemeyer@gmail.com>
2022-09-14 11:59:40 +0200

    added VoidReferencePointer
    VoidReferencePointer is the same a ReferencePointer but pointing to a void* instead of normal pointer

--- ECS/DataAccess/References.h
+++ ECS/DataAccess/References.h
@@ -2,6 +2,11 @@
 
 template <typename T> class Reference;
 
+/**
+ * Reference pointer is a class that always points to the element it is assigned to.
+ * It is static in memory and gets updated by the Type View in case the data has to change locations.
+ * In case the element it points to is deleted it will contain a nullptr and only be deallocated from memory once no references point to it.
+ */
 template <typename T>
 class ReferencePointer final
 {
@@ -25,6 +30,35 @@ private:
 	size_t m_Counter{};
 };
 
+class VoidReferencePointer final
+{
+public:
+	VoidReferencePointer(void* ptr);
+
+	VoidReferencePointer(const VoidReferencePointer&) = delete;
+	VoidReferencePointer& operator=(const VoidReferencePointer&) = delete;
+	VoidReferencePointer(VoidReferencePointer&&) = delete;
+	VoidReferencePointer& operator=(VoidReferencePointer&&) = delete;
+
+	static VoidReferencePointer InvalidRef() { return nullptr; }
+
+	bool IsValid() const { return m_ptr != nullptr; }
+	size_t GetReferencesAmount() const { return m_Counter; }
+
+	template <typename T>
+	ReferencePointer<T>& ToReferencePointer() { return reinterpret_cast<ReferencePointer<T>>(*this); }
+
+	void* m_ptr{};
+private:
+	size_t m_Counter{};
+};
+
+/**
+ * Reference is a class that points to a ReferencePointer which points at an element.
+ * It can be moved, deleted, copied, etc without losing the pointer to the original element.
+ * In case the element changes memory locations the Type View will update the ReferencePointer.
+ * Because the ReferencePointer is static in memory, a Reference will always be pointing to either a valid element or a nullptr.
+ */
 template <typename T>
 class Reference final
 {
@@ -39,16 +73,11 @@ public:
 
 	const ReferencePointer<T>& GetReferencePointer() const { return *m_ReferencePointer; }
 	ReferencePointer<T>& GetReferencePointer() { return *m_ReferencePointer; }
+
+	/** Check if the element has not been deleted yet*/
 	bool IsValid() const
 	{
-		if (m_ReferencePointer && m_ReferencePointer->m_ptr)
-			return true;
-		if (m_ReferencePointer && !m_ReferencePointer->m_ptr)
-		{
-			--m_ReferencePointer->m_Counter;
-			m_ReferencePointer = nullptr;
-		}
-		return false;
+		return (m_ReferencePointer && m_ReferencePointer->m_ptr);
 	}
 
 	static Reference InvalidRef() { return nullptr; }
@@ -57,11 +86,18 @@ private:
 	ReferencePointer<T>* m_ReferencePointer;
 };
 
+/**
+ * Same as a Reference but without a Type.
+ * It can be converted to any Reference<Type> but will result in undefined behaviour if not casted to the right type.
+ */
 class VoidReference final
 {
 public:
 	VoidReference(void* ReferencePointer) : m_ReferencePointer{ ReferencePointer } {}
 	VoidReference() = default;
+
+	//template <typename T>
+	//VoidReference(Reference<T>& ref) : m_ReferencePointer(&ref.GetReferencePointer()) {}
 
 	template <typename T>
 	const ReferencePointer<T>& GetReferencePointer() const { return *static_cast<const ReferencePointer<T>*>(m_ReferencePointer); }
@@ -72,6 +108,9 @@ public:
 		auto reference = static_cast<ReferencePointer<T>*>(m_ReferencePointer);
 		return *reference;
 	}
+
+	void* Data() { return GetReferencePointer<void>().m_ptr; }
+	const void* Data() const { return GetReferencePointer<void>().m_ptr; }
 
 private:
 	void* m_ReferencePointer{};
